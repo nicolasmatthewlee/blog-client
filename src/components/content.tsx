ac47ae2ddc3406fe59bb95f4d900f94c84e1041a
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-09 15:24:19 -0800

    added additional assets

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -3,9 +3,14 @@ import { ArticleBrief } from "./articlebrief";
 import IMG_1 from "../assets/1.jpg";
 import IMG_2 from "../assets/2.jpg";
 import IMG_3 from "../assets/3.jpg";
-import IMG_4 from "../assets/7.jpg";
+import IMG_4 from "../assets/4.jpg";
 import IMG_5 from "../assets/5.jpg";
 import IMG_6 from "../assets/6.jpg";
+import IMG_7 from "../assets/7.jpg";
+import IMG_8 from "../assets/8.jpg";
+import IMG_9 from "../assets/9.jpg";
+import IMG_10 from "../assets/10.jpg";
+import IMG_11 from "../assets/11.jpg";
 
 export const Content: Function = () => {
   return (
@@ -20,6 +25,11 @@ export const Content: Function = () => {
         <ArticleBrief img={IMG_4} />
         <ArticleBrief img={IMG_5} />
         <ArticleBrief img={IMG_6} />
+        <ArticleBrief img={IMG_7} />
+        <ArticleBrief img={IMG_8} />
+        <ArticleBrief img={IMG_9} />
+        <ArticleBrief img={IMG_10} />
+        <ArticleBrief img={IMG_11} />
       </div>
     </div>
   );
