72f38fbdf2b5c186d9e72caae81985b8f2154abe
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-09 10:40:10 -0800

    built trending articles section on recommended sidebar

--- src/components/recommended.tsx
+++ src/components/recommended.tsx
@@ -1,11 +1,12 @@
 import { TrendingUser } from "./trendinguser";
+import { TrendingArticle } from "./trendingarticle";
 
 export const Recommended: Function = () => {
   return (
-    <div className="hidden md:block w-72 border-l-2 p-4 pt-8 space-y-8">
+    <div className="hidden md:block w-72 border-l-2 p-4 pt-8 space-y-6">
       {/* trending */}
-      <div>
-        <div className="flex space-x-1">
+      <div className="border-b-2 pb-6">
+        <div className="flex space-x-1 items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -18,10 +19,15 @@ export const Recommended: Function = () => {
               clipRule="evenodd"
             />
           </svg>
-
-          <h3>Trending</h3>
+          <h3 className="font-medium text-gray-700 text-sm">Trending</h3>
+        </div>
+        <div className="mt-4 space-y-4">
+          <TrendingArticle />
+          <TrendingArticle />
+          <TrendingArticle />
         </div>
       </div>
+
       {/* popular authors */}
       <div className="border-b-2 pb-4">
         <div className="flex space-x-1 items-center">
@@ -49,8 +55,8 @@ export const Recommended: Function = () => {
       </div>
       {/* topics for you */}
 
-      <div>
-        <div className="flex space-x-1">
+      <div className="border-b-2 pb-4">
+        <div className="flex space-x-1 items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
@@ -60,7 +66,13 @@ export const Recommended: Function = () => {
             <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 00-1.032-.211 50.89 50.89 0 00-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 002.433 3.984L7.28 21.53A.75.75 0 016 21v-4.03a48.527 48.527 0 01-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979z" />
             <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 001.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0015.75 7.5z" />
           </svg>
-          <h3>Popular authors</h3>
+          <h3 className="font-medium text-gray-700 text-sm">Popular authors</h3>
+        </div>
+        <div className="mt-4 space-y-4">
+          <TrendingUser />
+          <TrendingUser />
+          <TrendingUser />
+          <TrendingUser />
         </div>
       </div>
     </div>
