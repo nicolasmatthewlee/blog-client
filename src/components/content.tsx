b7dab57f7d89ba160e011804b78cc2f829ab530a
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-09 09:57:05 -0800

    set content to split into two columns on xl and above

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -1,7 +1,10 @@
 import { ArticleBrief } from "./articlebrief";
 export const Content: Function = () => {
   return (
-    <div className="flex-1 p-4 space-y-4 bg-gray-100 overflow-scroll">
+    <div
+      className="flex-1 p-4 space-y-4 bg-gray-100 overflow-scroll
+    xl:columns-2"
+    >
       <ArticleBrief />
       <ArticleBrief />
       <ArticleBrief />
