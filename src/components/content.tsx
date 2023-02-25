c50dae62d5a79b9410dcc8e50bc02c32dfa46e6a
nicolasmatthewlee <nicolasmatthewlee@gmail.com>
2023-02-25 11:35:59 -0800

    Implemented see more feature: 1. added more button to Content, 2. created function specifically for getting the ID of articles to Show, which is called on mounting if type!=='saved' else sets articlesToShow=user.saved, 3. onClick more, fetch with optional URL parameter before encoding the current oldest retrieved article, 4. if return from articlesToShow is an empty array, set allArticlesShown=true (then hides more button)

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -5,7 +5,7 @@ import { Spinner } from "./spinner";
 const uniqid = require("uniqid");
 
 interface Props extends UserInterface {
-  type: string;
+  type: undefined | "saved";
   onUpdate: Function;
 }
 
@@ -23,116 +23,94 @@ interface Article {
 export const Content: Function = ({ type, user, onUpdate }: Props) => {
   const [errors, setErrors] = useState<any>(null);
   const [articles, setArticles] = useState<Article[]>([]);
+  const [articlesToShow, setArticlesToShow] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState<Boolean>(false);
+  const [allArticlesShown, setAllArticlesShown] = useState<Boolean>(false);
 
+  // 1. first retrieve list of articleIds to fetch details for
+  const getArticlesToShow = async (before?: string) => {
+    setIsLoading(true);
+    try {
+      const response = before
+        ? await fetch(
+            `http://127.0.0.1:5000/articles?` +
+              new URLSearchParams({
+                before: before,
+              })
+          )
+        : await fetch(`http://127.0.0.1:5000/articles`);
+
+      const json = await response.json();
+      if (json.errors) setErrors(json.errors);
+      if (json.length === 0) setAllArticlesShown(true);
+      else setArticlesToShow(json);
+    } catch (err) {
+      setErrors([{ message: "An unknown error occurred" }]);
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (type !== "saved") getArticlesToShow();
+    else setArticlesToShow(user ? user.saved : []);
+  }, []);
+
+  // 2. fetch details for each article in articleIds
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
-    if (type === "saved") {
-      const getSavedArticles = async () => {
-        setIsLoading(true);
-        try {
-          const urls = user?.saved.map(
-            (id) => "http://127.0.0.1:5000/articles/" + id
-          );
-          if (!urls) return;
+    const retrieveArticles = async () => {
+      setIsLoading(true);
+      const allArticleResponses = articlesToShow.map((id) =>
+        fetch(`http://127.0.0.1:5000/articles/${id}`, {
+          method: "get",
+          signal,
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((json) => {
+            if (json.errors) throw Error();
+            setArticles((articles) => articles.concat(json));
+            return true;
+          })
+          .catch(() => {
+            return "An unknown error occured";
+          })
+      );
 
-          const requests = urls.map((url) =>
-            fetch(url, {
-              method: "get",
-              signal,
-            })
-              .then((response) => {
-                return response.json();
-              })
-              .then((json) => {
-                if (json.errors) throw Error();
-                setArticles((articles) => articles.concat(json));
-                return true;
-              })
-              .catch((err) => {
-                return "An unknown error occured";
-              })
-          );
-          await Promise.allSettled(requests).then(() => {
-            if (!signal.aborted) setIsLoading(false);
-          });
-        } catch (err) {
-          if (!signal.aborted) {
-            setErrors([{ message: "An unknown error occurred" }]);
-            setIsLoading(false);
-          }
-        }
-      };
-      getSavedArticles();
-    } else {
-      const getAllArticles = async () => {
-        setIsLoading(true);
-        try {
-          const response = await fetch("http://127.0.0.1:5000/articles");
-          const json = await response.json();
-          if (json.errors) setErrors(json.errors);
-          else {
-            const articleIds: string[] = json;
-            const retrieveArticles = async () => {
-              const allArticleResponses = articleIds.map((id) =>
-                fetch(`http://127.0.0.1:5000/articles/${id}`, {
-                  method: "get",
-                  signal,
-                })
-                  .then((response) => {
-                    return response.json();
-                  })
-                  .then((json) => {
-                    if (json.errors) throw Error();
-                    setArticles((articles) => articles.concat(json));
-                    return true;
-                  })
-                  .catch((err) => {
-                    return "An unknown error occured";
-                  })
-              );
+      await Promise.allSettled(allArticleResponses).then(() => {
+        if (!signal.aborted) setIsLoading(false);
+      });
+    };
+    retrieveArticles();
 
-              await Promise.allSettled(allArticleResponses).then(() => {
-                if (!signal.aborted) setIsLoading(false);
-              });
-            };
-            retrieveArticles();
-          }
-        } catch (err) {
-          if (!signal.aborted) {
-            setErrors([{ message: "An unknown error occurred" }]);
-            setIsLoading(false);
-          }
-        }
-      };
-      getAllArticles();
-    }
-
+    // cleanup
     return () => controller.abort();
-  }, [type]);
+  }, [articlesToShow]);
 
   return (
     <div className="w-full">
-      <div className="xl:columns-2 space-y-4 flex flex-col items-center sm:inline-block">
-        {articles.map((a) => (
-          <ArticleBrief
-            key={a._id}
-            id={a._id}
-            image={a.image}
-            imageAlt={a.imageAlt}
-            title={a.title}
-            textBrief={a.textBrief}
-            author={a.author}
-            authorId={a.authorId}
-            created={a.created}
-            userId={user?._id}
-            saved={user?.saved.includes(a._id)}
-            liked={user?.liked.includes(a._id)}
-            onUpdate={onUpdate}
-          />
-        ))}
-      </div>
+      {articles.length > 0 ? (
+        <div className="xl:columns-2 space-y-4 flex flex-col items-center sm:inline-block">
+          {articles.map((a) => (
+            <ArticleBrief
+              key={a._id}
+              id={a._id}
+              image={a.image}
+              imageAlt={a.imageAlt}
+              title={a.title}
+              textBrief={a.textBrief}
+              author={a.author}
+              authorId={a.authorId}
+              created={a.created}
+              userId={user?._id}
+              saved={user?.saved.includes(a._id)}
+              liked={user?.liked.includes(a._id)}
+              onUpdate={onUpdate}
+            />
+          ))}
+        </div>
+      ) : null}
       {isLoading ? (
         <div className="pt-4">
           <Spinner />
@@ -147,7 +125,24 @@ export const Content: Function = ({ type, user, onUpdate }: Props) => {
         <h2 className="font-bold text-lg text-gray-400 w-full text-center">
           Nothing to show
         </h2>
-      ) : null}
+      ) : !allArticlesShown && type !== "saved" ? (
+        <div className="w-full py-8 flex items-center justify-center">
+          <button
+            className="bg-white font-medium py-1 px-6 text-gray-600 text-lg shadow text-center rounded-full
+          hover:bg-gray-50"
+            onClick={() => {
+              const oldestShown: string = articlesToShow.sort().slice(0, 1)[0];
+              getArticlesToShow(oldestShown);
+            }}
+          >
+            more
+          </button>
+        </div>
+      ) : (
+        <h2 className="font-bold text-lg text-gray-400 w-full text-center py-4">
+          No more to show
+        </h2>
+      )}
     </div>
   );
 };
